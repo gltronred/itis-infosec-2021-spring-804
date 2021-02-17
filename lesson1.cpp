dc29039af7c77d5e8972b23d6f93361e1ae5ac95
Mansur Ziiatdinov <gltronred@gmail.com>
2021-02-17 12:48:55 +0300

    Char* and task

--- lesson1.cpp
+++ lesson1.cpp
@@ -28,6 +28,63 @@ int collatz(int start) {
 // Напишите функцию
 // для перевода температуры в Фаренгейтах
 // в температуру в Кельвинах
+double ftok(int f) {
+  // преобразование типов
+  return (double)f;
+}
+
+// Массивы
+int sumPascal(int level) {
+  int row[100];
+  row[0] = 1;
+  for (int i=1; i<level; i++) {
+    for (int j=level; j>0; j--) {
+      row[j] = row[j-1] + row[j];
+    }
+  }
+  int sum = 0;
+  for (int i=0; i<level; i++) {
+    sum += row[i];
+  }
+  return sum;
+}
+
+void printRow(int n) {
+  int* a = new int[n];
+  a[0] = 1;
+  for (int i=1; i<n; i++) {
+    *(a+i) = 2*i + *(a+(i-1));
+    std::cout << a[i] << " ";
+  }
+  std::cout << std::endl;
+  char* b = (char*)a;
+  for (int i=0; i<4*n; i++) {
+    std::cout << (int)b[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+// Строки
+void printStr(int cnt) {
+  char* s = "abcdef";
+  char* t = "ghij";
+  std::cout << s << std::endl << t << std::endl;
+  char* u = new char[11];
+  for (int i=0; i<6; i++) {
+    u[i] = s[i];
+  }
+  for (int i=0; i<4; i++) {
+    u[i+6] = t[i];
+  }
+  u[10] = '\0';
+  std::cout << u << std::endl;
+}
+
+// Напишите функцию
+// для вывода строки в hex-формате
+void printHex(char* s){
+
+}
 
 // Точка входа:
 int main() {
@@ -40,5 +97,8 @@ int main() {
   // если надоело писать std::
   // в начале программы написать
   // using namespace std;
+  std::cout << sumPascal(10) << std::endl;
+  printRow(15);
+  printStr(0);
   return 0;
 }
